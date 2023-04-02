633673b902a19a95418728eb29fd9538ba55a991
dimakalchev <dima.kalchev2017@gmail.com>
2023-04-02 18:29:56 +0300

    list.functions

---
+++ Day_5/p1.py
@@ -0,0 +1,34 @@
+# fruits = ["Apples", "Peaches", "Pears"]
+# for fruit in fruits:
+#      print(fruit)
+# print(fruits)
+
+# 1 EXERCISE
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
+# total_height = 0
+#
+# for height in student_heights:
+#     total_height += height
+# print(total_height)
+# numbers_of_students = 0
+# for student in student_heights:
+#     numbers_of_students += 1
+# average_height = round(total_height / numbers_of_students)
+# print(average_height)
+
+# 2 EXERCISE
+# student_scores = input("Input a list students score ").split()
+#
+# highest_score = 0
+# for score in student_scores:
+#    if score > highest_score:
+#        highest_score = score
+# print(f"The highest score in the class is: {highest_score}")
+
+
+
+
+
+
